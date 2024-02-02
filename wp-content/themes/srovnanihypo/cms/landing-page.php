7969fb20c9c1280db874d52720c6fe07ec3eb6b6
Petrnemec98 <103196707+Petrnemec98@users.noreply.github.com>
2024-02-02 16:17:40 +0100

    Styly pro formulář

--- wp-content/themes/srovnanihypo/cms/landing-page.php
+++ wp-content/themes/srovnanihypo/cms/landing-page.php
@@ -4,21 +4,33 @@ get_header();
 ?>
 
 <!--Demo -->
-<h1><?=get_field('nadpis');?></h1>
-<h2><?=get_field('pod-nadpis');?></h2>
-<div class="intro">
-    <?=get_field('perex');?>
+<div class="section">
+    <div class="container">
+        <h1><?=get_field('nadpis');?></h1>
+        <h2><?=get_field('pod-nadpis');?></h2>
+        <div class="intro">
+            <?=get_field('perex');?>
+        </div>
+    </div>
 </div>
 
-<p>test</p>
 
-<div class="wysiwig">
-    <?=get_field('test');?>
+<div class="section dark">
+    <div class="container">
+        <div class="wysiwig">
+            <?=get_field('test');?>
+        </div>
+    </div>
 </div>
 
 
+<div class="section">
+    <div class="container">
+        <?php echo do_shortcode('[contact-form-7 id="3b9d0b0" title="Formulář"]'); ?>
+    </div>
+</div>
 
-<?php echo do_shortcode('[contact-form-7 id="3b9d0b0" title="Formulář"]'); ?>
+
 
 <?php
 get_footer();
