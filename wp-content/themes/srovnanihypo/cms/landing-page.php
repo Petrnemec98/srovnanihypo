6683d059537e1207a51fb9c8d96f18c2e9c70c22
Petrnemec98 <103196707+Petrnemec98@users.noreply.github.com>
2024-02-03 13:46:40 +0100

    add menu

--- wp-content/themes/srovnanihypo/cms/landing-page.php
+++ wp-content/themes/srovnanihypo/cms/landing-page.php
@@ -3,16 +3,35 @@
 get_header();
 ?>
 
-<!--Demo -->
-<div class="section">
+
+<div class="section homepage-header-block">
+    <div class="background-cover">
+        <div class="background-color"></div>
+        <div class="background-img"></div>
+    </div>
     <div class="container">
-        <h1><?=get_field('nadpis');?></h1>
-        <h2><?=get_field('pod-nadpis');?></h2>
-        <div class="intro">
+        <h1 class="light"><?=get_field('nadpis');?></h1>
+        <div class="intro light">
             <?=get_field('perex');?>
+        </div>
+        <div class="buttons">
+            <a href="#kalkulacka" class="btn primary">
+                Chci novou hypotéku
+            </a>
+            <a href="#kalkulacka" class="btn primary">
+                Chci refinancovat
+            </a>
         </div>
     </div>
 </div>
+<div class="section">
+    <div class="container">
+        <div class="wysiwig">
+            <?=get_field('test');?>
+        </div>
+    </div>
+</div>
+
 
 
 <div class="section dark">
@@ -25,7 +44,7 @@ get_header();
 
 
 <div class="section">
-    <div class="container">
+    <div id="kalkulacka" class="container">
         <?php echo do_shortcode('[contact-form-7 id="3b9d0b0" title="Formulář"]'); ?>
     </div>
 </div>
