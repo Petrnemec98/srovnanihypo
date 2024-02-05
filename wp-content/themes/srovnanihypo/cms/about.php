4c3112367267ee7a5747241d999f473d2b0192a3
Petrnemec98 <103196707+Petrnemec98@users.noreply.github.com>
2024-02-05 22:09:02 +0100

    Kontakt

--- wp-content/themes/srovnanihypo/cms/about.php
+++ wp-content/themes/srovnanihypo/cms/about.php
@@ -3,12 +3,32 @@
 get_header();
 ?>
 
-    <!--Demo -->
-    <h1><?=get_field('nadpis');?></h1>
-    <h2><?=get_field('pod-nadpis');?></h2>
-    <div class="intro">
-        <?=get_field('perex');?>
+<div class="section">
+    <div class="container intro-block">
+        <h1><?=get_field('nazev','option');?></h1>
+        <div class="detail">
+            <p>IČO: <?=get_field('adresa','option');?></p>
+            <p>IČO: <?=get_field('ico','option');?></p>
+            <p>IČO: <?=get_field('spisova_znacka','option');?></p>
+        </div>
+        <div class="buttons">
+            <a href="tel:<?=get_field('telefon','option');?>" class="btn secondary">
+                <?=get_field('telefon','option');?>
+            </a>
+            <a href="mailto:<?=get_field('email','option');?>" class="btn secondary">
+                <?=get_field('email','option');?>
+            </a>
+        </div>
+
     </div>
+</div>
+
+
+    <!--yellow banner-->
+    <?php include(get_template_directory() . "/cms/snippets/yellow-banner.php"); ?>
+
+    <!--Partneři-->
+    <?php include(get_template_directory() . "/cms/snippets/partneri-light.php"); ?>
 
 
 <?php
