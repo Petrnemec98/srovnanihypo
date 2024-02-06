54e13d43adf53edfe8120b94b434635709e3eec0
Petrnemec98 <103196707+Petrnemec98@users.noreply.github.com>
2024-02-06 18:50:44 +0100

    Add dalsi-produkty

---
+++ wp-content/themes/srovnanihypo/cms/Dalsi-produkty.php
@@ -0,0 +1,28 @@
+<?php
+/* Template Name: Dalši produkty - coffee digital*/
+get_header();
+?>
+
+<!--Demo -->
+
+<div class="section intro-block">
+    <div class="container">
+        <h1><h1><?=get_field('nadpis');?></h1></h1>
+    </div>
+</div>
+
+<div class="section">
+    <div class="container tiny">
+        <?=get_field('obsah');?>
+    </div>
+</div>
+
+<!--yellow banner-->
+<?php include(get_template_directory() . "/cms/snippets/yellow-banner.php"); ?>
+
+<!--Partneři-->
+<?php include(get_template_directory() . "/cms/snippets/partneri-light.php"); ?>
+
+<?php
+get_footer();
+?>
