3bfe2e9308cd71c69c291e88a52963cfcfab1bbc
Petrnemec98 <103196707+Petrnemec98@users.noreply.github.com>
2024-03-21 23:16:31 +0100

    skider fix

--- wp-content/themes/srovnanihypo/cms/snippets/partneri-light.php
+++ wp-content/themes/srovnanihypo/cms/snippets/partneri-light.php
@@ -1,15 +1,21 @@
 <div class="section">
     <div class="container partneri">
         <?php if (have_rows('partneri','option')) { ?>
-            <div class="row">
-                <?php while (have_rows('partneri','option')) { the_row(); ?>
-                    <div class="item">
-                        <a href="<?=get_sub_field('url');?>">
-                            <img src="<?=get_sub_field('img');?>">
-                        </a>
-                    </div>
-                <?php } ?>
+            <div class="row splide">
+                <div class="splide__track">
+                    <ul class="splide__list">
+                        <?php while (have_rows('partneri','option')) { the_row(); ?>
+
+                            <li class="splide__slide">
+                                <a href="<?=get_sub_field('url');?>">
+                                    <img src="<?=get_sub_field('img');?>">
+                                </a>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </div>
             </div>
         <?php } ?>
     </div>
+
 </div>
