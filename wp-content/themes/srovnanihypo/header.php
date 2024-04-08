7447be2fba25be7880f66c35bd750c785039c12a
Petrnemec98 <petr.nemec.1998@gmail.com>
2024-04-08 08:11:24 +0200

    remove favicon

--- wp-content/themes/srovnanihypo/header.php
+++ wp-content/themes/srovnanihypo/header.php
@@ -10,21 +10,6 @@
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Favicon -->
-        <link rel="apple-touch-icon" sizes="180x180"
-              href="<?= get_template_directory_uri(); ?>/static/favicon/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32"
-              href="<?= get_template_directory_uri(); ?>/static/favicon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16"
-              href="<?= get_template_directory_uri(); ?>/static/favicon/favicon-16x16.png">
-        <link rel="manifest" href="<?= get_template_directory_uri(); ?>/static/favicon/site.webmanifest">
-        <link rel="mask-icon" href="<?= get_template_directory_uri(); ?>/static/favicon/safari-pinned-tab.svg"
-              color="#5bbad5">
-        <link rel="shortcut icon" href="<?= get_template_directory_uri(); ?>/static/favicon/favicon.ico">
-        <meta name="msapplication-TileColor" content="#da532c">
-        <meta name="msapplication-config" content="<?= get_template_directory_uri(); ?>/static/favicon/browserconfig.xml">
-        <meta name="theme-color" content="#ffffff">
-
         <!-- CSS -->
         <link rel="stylesheet" type="text/css" href="<?= get_template_directory_uri(); ?>/static/css/styles.css">
 
